4572c54136473488a4ab0297da9ae267fe8776dd
dereck lhw <dereck.lamhonwah@gmail.com>
2024-02-20 01:55:52 +0400

    refac: change variable to constat: 7

--- src/digit_recognition/DatasetReader.java
+++ src/digit_recognition/DatasetReader.java
@@ -5,16 +5,16 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class DatasetReader {
-    private static final int dataSize = 2810;
-    private static final int singleInputSize = 65;
+    private static final int DATA_SIZE = 2810;
+    private static final int SINGLE_INPUT_SIZE = 65;
 
     public static int[][] readDataset(String filePath) throws FileNotFoundException {
-        int[][] result = new int[dataSize][singleInputSize];
+        int[][] result = new int[DATA_SIZE][SINGLE_INPUT_SIZE];
         try (Scanner scanner = new Scanner(new File(filePath))) {
             int resultIndex = 0;
             while (scanner.hasNextLine()) {
                 String[] splitLine = scanner.nextLine().split(",");
-                int[] dataLine = new int[singleInputSize];
+                int[] dataLine = new int[SINGLE_INPUT_SIZE];
                 for (int index = 0; index < splitLine.length; index++) {
                     dataLine[index] = Integer.parseInt(splitLine[index]);
                 }
